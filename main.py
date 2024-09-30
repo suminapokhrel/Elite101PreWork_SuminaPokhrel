3bfdd6214eab6ba9fc139117529af12234946c6d
Sumina Pokhrel <pokhrel.sumina66@gmail.com>
2024-09-30 01:47:15 +0000

    user input for their age and name.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 def welcome():
     print("HIII! Welcome to SumaChat, your friendly chatbot! 🎀")
     print("I'm here to make you smile and your day a little brighter! Before you can start using SumaChat, I have to get to know you!")
+
+def user_info():
+    name = input("What's your name? ")
+    age = input(f"What a beautiful name, {name}, it has a nice ring to it! And how old are you? ")
+    return name, age
