a010508f719d4911733d2f1ed3c79985aef21829
Sumina Pokhrel <pokhrel.sumina66@gmail.com>
2024-09-30 01:46:03 +0000

    welcomes user to SumaChat

---
+++ main.py
@@ -0,0 +1,3 @@
+def welcome():
+    print("HIII! Welcome to SumaChat, your friendly chatbot! 🎀")
+    print("I'm here to make you smile and your day a little brighter! Before you can start using SumaChat, I have to get to know you!")
