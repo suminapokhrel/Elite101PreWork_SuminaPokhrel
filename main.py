57af98b104e0453cefb4b94b8a144a3777772026
Sumina Pokhrel <pokhrel.sumina66@gmail.com>
2024-09-30 00:56:30 +0000

    program gets users name + age

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import random
 def welcome():
     print("HII! Welcome to Sumachat, your friendly chatbot!")
     print("I'm here to make a smile on your face and make your day a little bit brighter, so let's get started.")
-    
+
